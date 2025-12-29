cef0e1d2a255f6f5ae5f8acae91a94c4ae6d21cc
saarthack <ssarthak2003@outlook.com>
2025-12-29 22:10:05 +0530

    class8

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var arr = [10,20,30,40]
+// 1. localStorage.clear() - to clean localStorage
+// 2. localStorage.setItem("key","value") - to add new item or overwrite existing ones
+// 3. localStorage.getItem("key") - to get any item 
+// 4. localStorage.removeItem("key") - to remove any item 
 
-arr.splice(0,4)
 
-console.log(arr);
+// JSON.stringify : Array/object -> String
+// JSON.parse : String -> Array/object 
